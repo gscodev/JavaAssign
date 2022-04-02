be6b1f4d46b49ab8f6fc05857d0728a8eb06b2c6
gscodev <90572774+gscodev@users.noreply.github.com>
2022-04-02 22:36:37 +0100

    Update SearchEngineGUI.java

--- SearchEngineGUI.java
+++ SearchEngineGUI.java
@@ -1,5 +1,3 @@
-package search_engine;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -37,3 +35,53 @@ public class SearchEngineGUI implements ActionListener {
         inputSearch.setBounds(85, 50, 200, 30);
         inputSearch.setFont(textFont);
         inputSearch.setVisible(true);
+
+
+        searchButton.setBounds(315, 50, 100, 30);
+        searchButton.setFont(textFont);
+        searchButton.setBackground(new Color(173, 37, 51));
+        searchButton.setForeground(Color.white);
+        searchButton.setOpaque(true);
+        searchButton.setBorderPainted(false);
+        searchButton.setVisible(true);
+
+        searchButton.addActionListener(this);
+
+        searchResult.setBounds(85, 150, 330, 200);
+        searchResult.setFont(new Font("Adelle Sans Devanagari", Font.PLAIN, 14));
+        searchResult.setForeground(new Color(173, 37, 51));
+        searchResult.setVisible(true);
+
+        mainFrame.add(inputSearch);
+        mainFrame.add(searchButton);
+        mainFrame.add(searchResult);
+
+        searchEngine.readFile();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new SearchEngineGUI();
+    }
+
+    // Action performed when button is pressed
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchButton) {
+            String inputKeyword = inputSearch.getText().toLowerCase(Locale.ROOT);
+            searchResult.setText("");
+            try {
+                int i = 0;
+                String writeToTextArea = "Keyword is present in following files:-\n";
+                ArrayList<String> foundSearchKeywords = searchEngine.searchKeyword(inputKeyword);
+                for (String foundSearchKeyword : foundSearchKeywords) {
+                    writeToTextArea += (++i) + ".\t" + foundSearchKeyword + "\n";
+                }
+                searchResult.setText(writeToTextArea);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+}
+
