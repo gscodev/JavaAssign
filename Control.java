14202782c9405626093351052fc09f3133707977
gscodev <90572774+gscodev@users.noreply.github.com>
2022-04-14 14:00:18 +0100

    Create Control.java

---
+++ Control.java
@@ -0,0 +1,17 @@
+package lab8;
+
+import java.io.IOException;
+
+//import java.awt.event.ActionEvent;
+
+//import java.io.FileNotFoundException;
+
+public class Control {
+
+	public static void main(String[] args) {
+		
+		GUI gui = new GUI();
+		Files FileCheck = new Files();
+		
+	}
+}
